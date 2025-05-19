1c016721b6ae0a3ee07af79edad9d13eb7808ba6
Kirat-kaur8899 <133039839+Kirat-kaur8899@users.noreply.github.com>
2025-05-20 00:22:25 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+# Install Node.js (LTS version)
+curl -sL https://rpm.nodesource.com/setup_18.x | sudo -E bash -
+sudo yum install -y nodejs
 cd /home/ec2-user/resume-builder
 npm install
