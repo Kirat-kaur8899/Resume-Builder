b5ba9c8bb803b460f838e41cc0df08d7ed6e989c
Kirat-kaur8899 <133039839+Kirat-kaur8899@users.noreply.github.com>
2025-05-20 00:39:49 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,5 +3,10 @@
 # Install Node.js (LTS version)
 curl -sL https://rpm.nodesource.com/setup_18.x | sudo -E bash -
 sudo yum install -y nodejs
-sudo cd /home/ec2-user/resume-builder
-sudo npm install
+
+# Fix directory permissions
+sudo chown -R ec2-user:ec2-user /home/ec2-user/resume-builder
+
+# Run npm install as ec2-user
+sudo -u ec2-user bash -c "cd /home/ec2-user/resume-builder && npm install"
+
