7b0df6de6e20f12122ea79032ce638df60987238
Kirat-kaur8899 <133039839+Kirat-kaur8899@users.noreply.github.com>
2025-05-20 12:05:50 +0530

    Update About.jsx

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -3,8 +3,8 @@ import { Helmet } from 'react-helmet';
 
 const testimonials = [
     {
-        name: 'Muhammad Shan',
-        role: 'Full Stack Developer',
+        name: 'Kirat Kaur',
+        role: 'Developer',
         content:
             'A resume builder website is a web-based tool that allows users to create and customize a professional resume to their desired specifications. These websites typically provide templates for creating a resume.',
         avatar:
